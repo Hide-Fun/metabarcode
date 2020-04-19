5e256b686a07b10ed35bcc41bb582bb08c0529b7
Hide-Fun <58735730+Hide-Fun@users.noreply.github.com>
2020-04-19 09:00:26 -0700

    Update make_index.R
    Update make_index.R and add test files.

--- R/make_index.R
+++ R/make_index.R
@@ -8,31 +8,30 @@
 #' @param .index_2 index2 column
 #' @export
 make_index = function(.filename, .skip, .id, .index_1, .index_2) {
+  id <- rlang::enquo(.id)
+  index_1 <- rlang::enquo(.index_1)
+  index_2 <- rlang::enquo(.index_2)
   # load data.
   samplesheet <- readr::read_csv(.filename, skip = .skip)
   #
-  ID <- samplesheet %>%
-    dplyr::select(.id)
-  index_1 <- samplesheet %>%
-    dplyr::select(.index_1)
-  index_2 <- samplesheet %>%
-    dplyr::select(.index_2)
+  index <- samplesheet %>%
+    dplyr::select(!!.id, !!.index_1, !!index_2) %>%
+    dplyr::rename(ID := !!.id, index_1 := !!.index_1, index_2 := !!.index_2) %>%
+    dplyr::mutate(ID = as.character(ID),
+                  ID = stringr::str_c(">", ID)) %>%
+    tibble::add_column(index_num = seq(length.out = nrow(samplesheet), by = 2) + 1,
+                       ID_num = seq(length.out = nrow(samplesheet), by = 2))
   #
-  index <- dplyr::tibble(ID <- ID,
-                        index_1 <- index_1,
-                        index_2 <- index_2) %>%
-    dplyr::mutate(ID <- stringr::str_c(">", ID)) %>%
-    tibble::add_column(index_num <- seq(length.out = nrow(samplesheet), by = 2) + 1,
-                       ID_num <- seq(length.out = nrow(samplesheet), by = 2))
   index_arrange <- dplyr::tribble(
     ~num, ~index_1, ~index_2,
     index$index_num, index$index_1, index$index_2,
     index$ID_num, index$ID, index$ID) %>%
     tidyr::unnest(cols = c(num, index_1, index_2)) %>%
     dplyr::arrange(num)
+  #
   readr::write_excel_csv(index_arrange, "index_arrange.csv")
-  readr::write_tsv(index_arrange[,2], col_names <- F, "index_1.fasta")
-  readr::write_tsv(index_arrange[,3], col_names <- F, "index_2.fasta")
+  readr::write_tsv(index_arrange[,2], col_names = F, "index_1.fasta")
+  readr::write_tsv(index_arrange[,3], col_names = F, "index_2.fasta")
   return(samplesheet)
   return(index)
   return(index_arrange)
---
+++ R/test_mac.R
@@ -0,0 +1,2 @@
+test_make_index <- make_index("data/SampleSheet-1.csv", 18, "Sample_ID", "index", "index2")
+fasta_to_tibble()
