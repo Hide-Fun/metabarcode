66b33257a5eba80d70f35a9d4254fbb6e9688964
Hide-Fun <58735730+Hide-Fun@users.noreply.github.com>
2020-05-03 13:32:48 +0900

    Update load_ncbi_xml.R
    add .term

--- R/load_ncbi_xml.R
+++ R/load_ncbi_xml.R
@@ -4,20 +4,25 @@
 #' You should keep to the regulations of E-utilities.
 #' @param .id character, Genbank accession ID.
 #' @param .filename saved xml file name.
+#' @param .term logical, you understand E-Utilities term ?
 #' @export
-load_ncbi_xml = function(.id, .filename = "download.xml") {
-  id_list <- .id %>%
-    dplyr::select(accession_id) %>%
-    dplyr::pull()
-  # accession id
-  .ids <- stringr::str_c(id_list, collapse = ",")
-  # url
-  base_url <-
-    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id={.ids}&rettype=gb&retmode=xml"
-  # add accession id.
-  use_url <- glue::glue(base_url)
-  # download.
-  utils::download.file(use_url,
-                       destfile = here::here(.filename))
-  Sys.sleep(0.5)
+load_ncbi_xml = function(.id, .filename = "download.xml", .term = F) {
+  if(.term == F) {
+    stop()
+  } else {
+    id_list <- .id %>%
+      dplyr::select(accession_id) %>%
+      dplyr::pull()
+    # accession id
+    .ids <- stringr::str_c(id_list, collapse = ",")
+    # url
+    base_url <-
+      "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id={.ids}&rettype=gb&retmode=xml"
+    # add accession id.
+    use_url <- glue::glue(base_url)
+    # download.
+    utils::download.file(use_url,
+                         destfile = here::here(.filename))
+    Sys.sleep(0.5)
+  }
 }
