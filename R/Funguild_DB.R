774d86c47cf6087d7439070f74bad59567757bed
Hide-Fun <58735730+Hide-Fun@users.noreply.github.com>
2020-05-03 13:53:33 +0900

    remove example

--- R/Funguild_DB.R
+++ R/Funguild_DB.R
@@ -5,8 +5,7 @@
 #' Names of output file is Funguild_DB.txt.
 #' @param .otu_table sumary table.
 #' @param .identify_list identifying result.
-#' @example
-#' Funguild_DB(otu_table, identidy_table)
+#' @examples
 Funguild_DB = function(.otu_table, .identify_list) {
   table <- .otu_table
   group <-  .identify_list
